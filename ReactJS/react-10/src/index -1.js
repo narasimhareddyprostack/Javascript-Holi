350107f2ca6474c7e1a5ce0cca55a205818abf88
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-04-13 11:02:56 +0530

    gtest

---
+++ ReactJS/react-10/src/index -1.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+ReactDOM.render("Hello,GA", document.getElementById('root'))
+//ReactDOM.render(1,2)
+//ReactDOM.render("What you want to reander","Where you want ot render")
---
+++ ReactJS/react-10/src/index-2.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+ReactDOM.render(<div>
+    <h1>Hello,Welcome to React World</h1>
+    <h2>Good Morning</h2>
+</div>, document.getElementById('root'))
+//ReactDOM.render("Hello,GA", document.getElementById('root'))
+
