86c1a96d15952d243a71a44cb062fe826edaf2aa
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-05-04 10:53:55 +0530

    okie

--- ReactJS/react-10/src/UserApp/UserApp.jsx
+++ ReactJS/react-10/src/UserApp/UserApp.jsx
@@ -5,7 +5,11 @@ import UserDetails from './UserDetails'
 class UserApp extends Component {
     constructor(props) {
         super(props)
-        this.state = { user: {} }
+        this.state = { user: {}, selectedUser: {} }
+    }
+    selectedUserMethod = (user) => {
+        console.log(user.firstName)
+        this.setState({ selectedUser: user })
     }
     componentDidMount() {
         Axios.get('https://dummyjson.com/users')
@@ -18,20 +22,25 @@ class UserApp extends Component {
         return (
             <div className='container'>
                 <h1>UserApp</h1>
-                <pre>{JSON.stringify(this.state.user)}</pre>
-
+                {/*   <pre>{JSON.stringify(this.state.user)}</pre>
+                <pre>{JSON.stringify(this.state.selectedUser)}</pre>
+ */}
 
                 <div className="row">
                     <div className="col-md-8">
                         {
                             Object.keys(this.state.user).length > 0 ? <>
-                                <UserList users={this.state.user.users} />
+                                <UserList users={this.state.user.users} selUser={this.selectedUserMethod} />
                             </> : null
                         }
 
                     </div>
                     <div className="col-md-4">
-                        <UserDetails />
+                        {
+                            Object.keys(this.state.selectedUser).length > 0 ? <>
+                                <UserDetails user={this.state.selectedUser} /></> : null
+                        }
+
                     </div>
                 </div>
 
--- ReactJS/react-10/src/UserApp/UserList.jsx
+++ ReactJS/react-10/src/UserApp/UserList.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
 class UserList extends Component {
+    selectedUserHandler = (user) => {
+        //console.log("Buddy Clicked", user.firstName)
+        this.props.selUser(user)
+    }
     render() {
         return (
             <div className='container'>
                 <h2>UserList</h2>
-                <pre>{JSON.stringify(this.props.users)}</pre>
+                {/*      <pre>{JSON.stringify(this.props.users)}</pre>
 
-
+ */}
                 <div className="row">
                     <div className="col">
                         <table className='table'>
@@ -22,7 +26,7 @@ class UserList extends Component {
 
                                 {
                                     this.props.users.map((user) => {
-                                        return <tr key={user.id}>
+                                        return <tr key={user.id} onMouseOver={this.selectedUserHandler.bind(this, user)}>
                                             <td>{user.id}</td>
                                             <td>{user.firstName}</td>
                                             <td>{user.email}</td>
